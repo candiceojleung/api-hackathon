e35d6da13877cb7ef5f45f0c0695c1a61b3384d6
candiceojleung <candice.leung@live.ca>
2025-02-08 12:40:20 -0500

    make code design and styling more consistent using prettier

--- scripts/index.js
+++ scripts/index.js
@@ -12,55 +12,58 @@ for (let i = 0; i < 6; i++) {
   //Front Card Image
   const frontImg = document.createElement("div");
   frontImg.classList.add("img-container");
-  frontImg.innerHTML = '<img class="card-front__img" src="./assets/images/ron.png" alt="ron outline">';
+  frontImg.innerHTML =
+    '<img class="card-front__img" src="./assets/images/ron.png" alt="ron outline">';
   cardFront.append(frontImg);
 
-   // Card-Back
-   const cardBack = document.createElement("div");
-   cardBack.classList.add("card-back");
+  // Card-Back
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("card-back");
 
-   const quoteText = document.createElement("p");
-   quoteText.classList.add("quote-text");
+  const quoteText = document.createElement("p");
+  quoteText.classList.add("quote-text");
 
-   // Append to front/back
-   cardBack.appendChild(quoteText);
-   card.appendChild(cardFront);
-   card.appendChild(cardBack);
+  // Append to front/back
+  cardBack.appendChild(quoteText);
+  card.appendChild(cardFront);
+  card.appendChild(cardBack);
 
-   card.addEventListener("click", (event) => {
-     const clickedCard = event.target.closest('.card');
-     handleCardClick(clickedCard);
-   });
+  card.addEventListener("click", (event) => {
+    const clickedCard = event.target.closest(".card");
+    handleCardClick(clickedCard);
+  });
 
-   container.appendChild(card);
- }
+  container.appendChild(card);
+}
 
- async function handleCardClick(clickedCard){
-    try{
-        const flippedCard=document.querySelector(".card.flipped"); 
-        if(flippedCard && flippedCard !== clickedCard){
-            flippedCard.classList.remove("flipped"); 
-        }
-
-        //flip clicked card
-        if (!clickedCard.classList.contains ("flipped")){
-            await getRonSwansonQuote(clickedCard); 
-            clickedCard.classList.add("flipped"); 
-        } else{
-            clickedCard.classList.remove("flipped");
-        }
-    }catch(error){
-            console.log("Error Fetching Quote:", error);
-        }
+async function handleCardClick(clickedCard) {
+  try {
+    const flippedCard = document.querySelector(".card.flipped");
+    if (flippedCard && flippedCard !== clickedCard) {
+      flippedCard.classList.remove("flipped");
     }
 
-    //get quote
- async function getRonSwansonQuote(card) {
-    try{
-      const response = await axios.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+    //flip clicked card
+    if (!clickedCard.classList.contains("flipped")) {
+      await getRonSwansonQuote(clickedCard);
+      clickedCard.classList.add("flipped");
+    } else {
+      clickedCard.classList.remove("flipped");
+    }
+  } catch (error) {
+    console.log("Error Fetching Quote:", error);
+  }
+}
+
+//get quote
+async function getRonSwansonQuote(card) {
+  try {
+    const response = await axios.get(
+      "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+    );
     const data = await response.data;
     card.querySelector(".quote-text").textContent = `"${data[0]}"`;
   } catch (error) {
-  console.log(error);
+    console.log(error);
   }
 }
